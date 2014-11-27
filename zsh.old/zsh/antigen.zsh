407e8b29596df430e428cc01a306111e14fcb540
solorabbit <cn_amb@yahoo.co.jp>
2014-11-27 22:15:47 +0900

    delete zsh.old

--- zsh.old/zsh/antigen.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-
-source ~/share/zsh/antigen/antigen.zsh clone/antigen.zsh
-antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle ~/share/zsh/completion
-antigen theme ~/share/zsh/theme.zsh
-antigen apply
